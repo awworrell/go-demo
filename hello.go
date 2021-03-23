0d363b3b57be2b8ef375afa2f549e5548bae3eed
Allen Worrell <allen.worrell@coxautoinc.com>
2021-03-23 09:44:39 -0400

    updating commnets for go mod tidy

--- hello.go
+++ hello.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 //importing the quote module from pkg.go.dev
+//go mod tidy downloaded the rsc.io/quote module
 import "rsc.io/quote"
-
 
 func main() {
     fmt.Println(quote.Go())
